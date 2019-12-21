641519d06a2c6a1827f3a59c7ffc653ada594bb9
YuriiChmutov <vad4807@gmail.com>
2019-12-21 12:45:19 +0200

    epamHomeTask4 | genericsAndCollections| done

---
+++ Epam.HomeTask4/Epam.HomeTask4/AnimalsFolder/Bear.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EPAM.HomeTask4
+{
+    class Bear: Animal
+    {
+        public Bear()
+        {
+            nameOfAnimal = "медведь";
+            isAppear = isAppearMethod();
+        }
+
+        public override string generateWayToTravelToTeremok()
+        {
+            List<string> ways = new List<string>()
+            {
+                "Прибежал", "Прискакал", "Прилетел частным авиорейсом",
+                "Подъехал на такси", "Пришел прогулочным шагом", "Подкрался"
+            };
+            
+            return ways[GetRandom.returnRandom(ways.Count)];
+        }
+    }
+}
---
+++ Epam.HomeTask4/Epam.HomeTask4/AnimalsFolder/IAnimal.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EPAM.HomeTask4
+{
+    interface IAnimal<T, B>
+    {
+        T whatToDo(ref int value); // не захочет | захочет жить в теремке
+        T generateWayToTravelToTeremok(); // каким способом животное пришло к теремку
+        T generatePhrase(); // что скажет животное новому животному (передаю полю)
+
+        B isAppearMethod(); // появится ли животное в сказке (у всех, кроме динозавра, вероятность 100% появиться)
+
+    }
+}
